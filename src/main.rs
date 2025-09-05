2963c129665f064fe360120a8813157669b21bcf
Jakob Staudinger <jakobstaudi@gmail.com>
2025-09-05 22:33:22 +0200

    control LEDs with PIO

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use core::cmp::Ordering;
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures::join::join3;
 use embassy_rp::{
     self, Peripheral, bind_interrupts,
     clocks::clk_sys_freq,
@@ -12,9 +13,9 @@ use embassy_rp::{
     pac::{self, SIO, dma::regs::CtrlTrig},
     peripherals::PIO1,
     pio::{Config, InterruptHandler, Pio, ShiftConfig},
-    pwm::{self, Pwm},
+    pwm::{self, Pwm, Slice},
 };
-use embassy_time::{Duration, Instant};
+use embassy_time::{Duration, Instant, Timer};
 use fixed::{FixedU32, types::extra::U8};
 use heapless::Vec;
 use itertools::{Either, Itertools, Merge};
@@ -84,15 +85,29 @@ impl PartialEq for Edge {
 impl Eq for Edge {}
 
 impl Edge {
-    pub fn encode(self, last_edge: Option<Edge>) -> u32 {
+    pub fn encode_red(&self, last_edge: &Option<Edge>) -> u16 {
+        self.encode(last_edge, |color| color.r())
+    }
+
+    pub fn encode_green(&self, last_edge: &Option<Edge>) -> u16 {
+        self.encode(last_edge, |color| color.g())
+    }
+
+    pub fn encode_blue(&self, last_edge: &Option<Edge>) -> u16 {
+        self.encode(last_edge, |color| color.b())
+    }
+
+    fn encode(
+        &self,
+        last_edge: &Option<Edge>,
+        get_color_component: impl FnOnce(Color) -> u8,
+    ) -> u16 {
         let delay = self.time - last_edge.map(|edge| edge.time).unwrap_or(Instant::MIN);
-        let delay = (delay.as_millis() / 10) as u32 & 0xFF;
+        let delay = (delay.as_millis() / 10) as u16 & 0xFF;
 
         match self.direction {
-            EdgeDirection::Falling => 0x00000000 | delay,
-            EdgeDirection::Rising(color) => {
-                (color.0 as u32) << 24 | (color.1 as u32) << 16 | (color.2 as u32) << 8 | delay
-            }
+            EdgeDirection::Falling => 0x0000_u16 | delay,
+            EdgeDirection::Rising(color) => (get_color_component(color) as u16) << 8 | delay,
         }
     }
 }
@@ -165,7 +180,7 @@ impl Iterator for StreamConfigIterator {
 }
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let locked_state = SIO.spinlock_st();
     info!("Spinlock state {:b}", locked_state.read());
 
@@ -175,20 +190,19 @@ async fn main(spawner: Spawner) {
 
     let mut pio = Pio::new(p.PIO1, Irqs);
 
-    // let red_pin = pio.common.make_pio_pin(p.PIN_2);
-    // let green_pin = pio.common.make_pio_pin(p.PIN_3);
-    // let blue_pin = pio.common.make_pio_pin(p.PIN_4);
+    let red_pin = p.PIN_2;
+    let green_pin = p.PIN_4;
+    let blue_pin = p.PIN_6;
 
     let timing_program = pio_asm! {
         r#"
             set y 0
+            wait 0 irq 0
 
         .wrap_target
-            pull
-            out x 24
-            in x 24
+            out x 8
+            in y 8
             in x 8
-            push
             out x 8
         wait1:
             jmp x-- wait1 [19]
@@ -203,74 +217,135 @@ async fn main(spawner: Spawner) {
     timing_config.clock_divider = FixedU32::<U8>::checked_from_num(clock_divider).unwrap();
     timing_config.shift_out = ShiftConfig {
         direction: embassy_rp::pio::ShiftDirection::Left,
-        auto_fill: false,
-        threshold: 0,
+        auto_fill: true,
+        threshold: 16,
     };
     timing_config.shift_in = ShiftConfig {
         direction: embassy_rp::pio::ShiftDirection::Left,
-        auto_fill: false,
-        threshold: 0,
+        auto_fill: true,
+        threshold: 16,
     };
 
-    let mut sm = pio.sm0;
-    sm.set_config(&timing_config);
-    sm.set_enable(true);
+    let mut pwm_config = pwm::Config::default();
+    pwm_config.enable = true;
+    pwm_config.top = 254;
 
-    let tx = sm.tx();
-    let mut config = pwm::Config::default();
-    config.enable = false;
-    let pwm = Pwm::new_output_a(p.PWM_SLICE2, p.PIN_4, config);
+    let pwm_slice_red = p.PWM_SLICE1.number();
+    let pwm = Pwm::new_output_a(p.PWM_SLICE1, red_pin, pwm_config.clone());
+    core::mem::forget(pwm);
 
-    spawner.must_spawn(sync_pio_to_pwm(
+    sync_pio_to_pwm(
         [p.DMA_CH0.degrade(), p.DMA_CH1.degrade()],
-        pwm,
-    ));
+        pwm_slice_red,
+        PwmChannel::A,
+        1,
+        0,
+    );
 
-    let mut stream_iter = StreamConfig::new(
-        Color(255, 255, 255),
-        Hz(1.),
-        Duration::from_millis(400),
-        None,
-    )
-    .into_iter();
+    let pwm_slice_green = p.PWM_SLICE2.number();
+    let pwm = Pwm::new_output_a(p.PWM_SLICE2, green_pin, pwm_config.clone());
+    core::mem::forget(pwm);
 
-    let mut dma = p.DMA_CH2.into_ref();
+    sync_pio_to_pwm(
+        [p.DMA_CH2.degrade(), p.DMA_CH3.degrade()],
+        pwm_slice_green,
+        PwmChannel::A,
+        1,
+        1,
+    );
 
-    let mut data: Vec<u32, 256> = Vec::new();
+    let pwm_slice_blue = p.PWM_SLICE3.number();
+    let pwm = Pwm::new_output_a(p.PWM_SLICE3, blue_pin, pwm_config.clone());
+    core::mem::forget(pwm);
+
+    sync_pio_to_pwm(
+        [p.DMA_CH4.degrade(), p.DMA_CH5.degrade()],
+        pwm_slice_blue,
+        PwmChannel::A,
+        1,
+        2,
+    );
+
+    let mut stream_iter =
+        StreamConfig::new(Color(10, 0, 0), Hz(1.), Duration::from_millis(400), None).into_iter();
+
     let mut last_edge = None;
+
+    pio.sm0.set_config(&timing_config);
+    pio.sm0.set_enable(true);
+
+    pio.sm1.set_config(&timing_config);
+    pio.sm1.set_enable(true);
+
+    pio.sm2.set_config(&timing_config);
+    pio.sm2.set_enable(true);
+
+    let mut dmas = (
+        p.DMA_CH7.into_ref(),
+        p.DMA_CH8.into_ref(),
+        p.DMA_CH9.into_ref(),
+    );
+    let mut red_data: Vec<u16, 256> = Vec::new();
+    let mut green_data: Vec<u16, 256> = Vec::new();
+    let mut blue_data: Vec<u16, 256> = Vec::new();
+
+    pio.irq_flags.set_all(0);
 
     loop {
         info!("Loop");
-        data.clear();
-        while !data.is_full() {
+        red_data.clear();
+        green_data.clear();
+        blue_data.clear();
+        while !red_data.is_full() {
             let edge = stream_iter.next().unwrap();
-            data.push(edge.encode(last_edge)).unwrap();
-            info!("{:x}", edge.encode(last_edge));
+            red_data.push(edge.encode_red(&last_edge)).unwrap();
+            green_data.push(edge.encode_green(&last_edge)).unwrap();
+            blue_data.push(edge.encode_blue(&last_edge)).unwrap();
             last_edge = Some(edge);
         }
         info!("Push");
-        tx.dma_push(dma.reborrow(), &data).await;
+        join3(
+            pio.sm0.tx().dma_push(dmas.0.reborrow(), &red_data),
+            pio.sm1.tx().dma_push(dmas.1.reborrow(), &green_data),
+            pio.sm2.tx().dma_push(dmas.2.reborrow(), &blue_data),
+        )
+        .await;
     }
 }
 
-#[embassy_executor::task]
-async fn sync_pio_to_pwm(dmas: [AnyChannel; 2], mut pwm: Pwm<'static>) {
-    let raw_pwm = pac::PWM.ch(2);
+enum PwmChannel {
+    A,
+    B,
+}
 
-    let pio_no = 1;
-    let sm = 0;
+fn sync_pio_to_pwm(
+    dmas: [AnyChannel; 2],
+    pwm_slice: usize,
+    channel: PwmChannel,
+    pio_number: u8,
+    sm: u8,
+) {
+    let raw_pwm = pac::PWM.ch(pwm_slice);
+
+    let treq_sel = pac::dma::vals::TreqSel::from(pio_number * 8 + sm + 4);
 
     let [first_dma, second_dma] = dmas;
     let r = first_dma.regs();
 
-    r.write_addr().write_value(raw_pwm.cc().as_ptr() as u32);
-    r.read_addr().write_value(pac::PIO1.rxf(0).as_ptr() as u32);
+    let offset = match channel {
+        PwmChannel::A => 0,
+        PwmChannel::B => 2,
+    };
+
+    r.write_addr()
+        .write_value(raw_pwm.cc().as_ptr() as u32 + offset);
+    r.read_addr()
+        .write_value(pac::PIO1.rxf(sm as usize).as_ptr() as u32);
     r.trans_count().write_value(u32::MAX);
     r.al1_ctrl().write(|val| {
         let mut w = CtrlTrig(0);
-        // Set RX DREQ for this statemachine
-        w.set_treq_sel(crate::pac::dma::vals::TreqSel::from(pio_no * 8 + sm + 4));
-        w.set_data_size(u16::size());
+        w.set_treq_sel(treq_sel);
+        w.set_data_size(u8::size());
         w.set_chain_to(second_dma.number());
         w.set_incr_read(false);
         w.set_incr_write(false);
@@ -281,23 +356,17 @@ async fn sync_pio_to_pwm(dmas: [AnyChannel; 2], mut pwm: Pwm<'static>) {
 
     let r = second_dma.regs();
 
-    r.write_addr().write_value(raw_pwm.cc().as_ptr() as u32);
-    r.read_addr().write_value(pac::PIO1.rxf(0).as_ptr() as u32);
+    r.write_addr()
+        .write_value(raw_pwm.cc().as_ptr() as u32 + offset);
+    r.read_addr()
+        .write_value(pac::PIO1.rxf(sm as usize).as_ptr() as u32);
     r.trans_count().write_value(u32::MAX);
     r.ctrl_trig().write(|w| {
-        // Set RX DREQ for this statemachine
-        w.set_treq_sel(crate::pac::dma::vals::TreqSel::from(pio_no * 8 + sm + 4));
-        w.set_data_size(u16::size());
+        w.set_treq_sel(treq_sel);
+        w.set_data_size(u8::size());
         w.set_chain_to(first_dma.number());
         w.set_incr_read(false);
         w.set_incr_write(false);
         w.set_en(true);
     });
-
-    let mut config = pwm::Config::default();
-    config.top = 256;
-    config.compare_a = 0;
-    config.enable = true;
-
-    pwm.set_config(&config);
 }
